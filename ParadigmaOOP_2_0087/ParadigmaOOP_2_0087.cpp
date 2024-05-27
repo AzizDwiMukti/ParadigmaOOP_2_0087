6785fb32c23bb2e1fd9957ae1cc5d490b46e1c48
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-27 14:38:51 +0700

    class lia

--- ParadigmaOOP_2_0087/ParadigmaOOP_2_0087.cpp
+++ ParadigmaOOP_2_0087/ParadigmaOOP_2_0087.cpp
@@ -3,8 +3,22 @@ using namespace std;
 
 class seseorang {
 public:
-	virtual void pesa() + 0;
+	virtual void pesa() = 0;
 	//virtual void pesan() {
 	//cout<<"Pesan dari seseorang"<<endl;
 	//}
+};
+
+class joko :public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari joko" << endl;
+	}
+};
+
+class Lia :public seseorang {
+public:
+		void pesan() {
+		cout << "Pesan dari lia" << endl;
+	}
 };
