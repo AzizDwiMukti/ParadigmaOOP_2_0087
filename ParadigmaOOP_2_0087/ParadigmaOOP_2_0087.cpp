c1d7f5d3c7b7a4266d7f0fb76caa1d2953a4ff56
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-27 14:47:32 +0700

    membuat class baseClass

--- ParadigmaOOP_2_0087/ParadigmaOOP_2_0087.cpp
+++ ParadigmaOOP_2_0087/ParadigmaOOP_2_0087.cpp
@@ -16,9 +16,23 @@ public:
 	}
 };
 
-class Lia :public seseorang {
+class lia :public seseorang {
 public:
-		void pesan() {
-		cout << "Pesan dari lia" << endl;
+	void pesan() {
+		cout << "pesan dari lia" << endl;
 	}
 };
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+
+	return 0;
+}
