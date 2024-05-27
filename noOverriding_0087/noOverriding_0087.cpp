cb314b9ce033f4da7f777bbed04050165b8328d9
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-27 14:50:24 +0700

    akses modifier base class

--- noOverriding_0087/noOverriding_0087.cpp
+++ noOverriding_0087/noOverriding_0087.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 class baseClass {
-
+public:
+	virtual void perkenalan() final {
+		cout << "Hallo saya function dari base class";
+	}
+};
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
+	}
 };
